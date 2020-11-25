cd8a431ad2c0337241a993fe9fbf7fd50af8c424
KuznetsovR <74103648+KuznetsovR@users.noreply.github.com>
2020-11-25 23:14:07 +0500

    Update seaBattle.js

--- seabattle/seaBattle.js
+++ seabattle/seaBattle.js
@@ -4,6 +4,16 @@ function getRandomInt(max) {
 
 let table = {
     field:[],
+    arr:[
+        [-1,-1],
+        [-1,0],
+        [-1,1],
+        [0,-1],
+        [0,1],
+        [1,-1],
+        [1,0],
+        [1,1]
+    ],
     makeElements: function(){                           // делает элементы, которые являются координатами
         for (i=0;i<10;i++){
             this.field[i] = [];    
@@ -11,18 +21,32 @@ let table = {
                 this.field[i][j] = 'none';
             }
         }
-        console.log(this.field); 
+        console.log(this.field);
+        console.log(typeof(this.field));
     },
-
+    shipChecker: function(coordinates){
+        let digit = coordinates.replace(/\D/g, "")-1;
+        let letter = coordinates.replace(/[^а-я]/gi, ""); 
+        let alphabet = 'абвгдежзиклмнопрстуфхцчшщъыьэюя';
+        letter = alphabet.indexOf(letter);
+        console.log(digit);
+        console.log(letter);
+        if (this.field[digit][letter]==="ship"){
+            ship.status === 0;
+            console.log('killed');
+        } else {
+            console.log('Miss');
+        }
+    },
     shipMaker: function(){
         
-        do {
+        //do {
         let column = getRandomInt(9);
         let row = getRandomInt(9);
         console.log(column);
         console.log(row);
-        } while (this.field[column][row] !== 'none');       //problem
-        this.field[column][row] = 'ship';
+        //} while (this.field[column][row] !== 'none');       //problem
+        this.field[row][column] = 'ship';
         
 
         //let digit = coordinates.replace(/\D/g, "");
@@ -35,19 +59,11 @@ let table = {
     },
     buiMaker: function(column, row){
         let x = 0;
-        let buis = [
-            [-1,-1],
-            [-1,0],
-            [-1,1],
-            [0,-1],
-            [0,1],
-            [1,-1],
-            [1,0],
-            [1,1]
-        ]
-        for(i=0;i<9;i++){
-            x = buis[i][0];     //problem
-            y = buis[i][1];
+        
+        console.log(typeof(this.arr));
+        for(p=0;p<9;p++){
+            x = this.arr[p][0];     //problem
+            y = this.arr[p][1];
             console.log(x);
             if (((0<=column+x)&&(column+x<10)) && ((0<=column+y)&&(column+y<10))) {
                 this.field[column+x][row+y] = 'bui';
@@ -80,7 +96,6 @@ let ship = {
         }
     },
     setCoordinates: function(value){
-        
         digits = parseInt(digits);
         if(digits<=10&&alphabet.indexOf(letters)<=9){
             this.coordinates = digits + letters;
@@ -92,7 +107,8 @@ let ship = {
 }
 
 table.makeElements();
-table.buiMaker(4, 4);
-//table.shipMaker();
-console.log(table.field);
+table.shipMaker();
+table.shipChecker('1б')
+//table.buiMaker(4, 4);
+//console.log(table.field);
 //table.setShip();
